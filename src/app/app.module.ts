91910bc6ad9657bd785e42b603badb4bcab63457
deejayy <deejayy@deejayy.hu>
2023-02-11 20:55:23 +0100

    Adjust rules, add ngrx

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreModule } from '@ngrx/store';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [AppRoutingModule, BrowserModule],
+  imports: [AppRoutingModule, BrowserModule, StoreModule.forRoot(), EffectsModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/setup-jest.ts
+++ src/setup-jest.ts
@@ -1,4 +1,3 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 import 'jest-preset-angular/setup-jest';
 import '@angular/localize/init';
 import './jest-global-mocks';
