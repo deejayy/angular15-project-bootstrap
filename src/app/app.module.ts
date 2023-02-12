7791d18dcc1f2923e3c6c62ad86c908d4e5c3c2e
deejayy <deejayy@deejayy.hu>
2023-02-12 23:04:32 +0100

    Add reactive config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,32 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { ReactiveConfigModule } from '@deejayy/reactive-config';
+import { environment } from '@env/environment';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
+export class ConfigVars {
+  public apiUrl!: string;
+}
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [AppRoutingModule, BrowserModule, StoreModule.forRoot(), EffectsModule.forRoot()],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    StoreModule.forRoot(),
+    EffectsModule.forRoot(),
+    StoreDevtoolsModule.instrument({
+      name: 'Angular 14 Bootstrap',
+      maxAge: 200,
+      logOnly: environment.production,
+    }),
+    ReactiveConfigModule.forRoot(ConfigVars, { configPath: environment.config }),
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
