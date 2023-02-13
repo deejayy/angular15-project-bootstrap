db97e264ec55d089d1739b986d767029ab3fd745
deejayy <deejayy@deejayy.hu>
2023-02-13 20:04:17 +0100

    Update configs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -15,19 +15,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app).toBeTruthy();
   });
-
-  it(`should have as title 'angular15-project-bootstrap'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular15-project-bootstrap');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain(
-      'angular15-project-bootstrap app is running!',
-    );
-  });
 });
