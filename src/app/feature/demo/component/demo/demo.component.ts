05c991f7be4941c3b27c8fc811ec38b162b3bad4
deejayy <deejayy@deejayy.hu>
2023-02-19 20:39:40 +0100

    Add api-caller example

--- src/app/feature/demo/component/demo/demo.component.ts
+++ src/app/feature/demo/component/demo/demo.component.ts
@@ -1,6 +1,31 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ApiCallerService } from '@deejayy/api-caller';
 import { DemoFacade } from '@feature/demo/store/demo.facade';
-import { Observable } from 'rxjs';
+import { Observable, of } from 'rxjs';
+
+interface DataModel {
+  id: number;
+  name: string;
+  username: string;
+  email: string;
+  address: {
+    street: string;
+    suite: string;
+    city: string;
+    zipcode: string;
+    geo: {
+      lat: string;
+      lng: string;
+    };
+  };
+  phone: string;
+  website: string;
+  company: {
+    name: string;
+    catchPhrase: string;
+    bs: string;
+  };
+}
 
 @Component({
   selector: 'app-demo',
@@ -11,9 +36,24 @@ import { Observable } from 'rxjs';
 export class DemoComponent {
   public date$: Observable<Date> = this.demoFacade.date$;
 
-  constructor(private demoFacade: DemoFacade) {}
+  public displayedColumns: string[] = ['id', 'name', 'username', 'email', 'address', 'phone', 'website', 'company'];
+
+  public data$: Observable<DataModel[]> = of([]);
+
+  private simpleCallItem = {
+    api: 'https://jsonplaceholder.typicode.com/',
+    path: 'users#1',
+  };
+
+  constructor(private demoFacade: DemoFacade, private apiCallerService: ApiCallerService) {
+    this.data$ = this.apiCallerService.createApiResults<DataModel[]>(this.simpleCallItem).data$;
+  }
 
   public updateState() {
     this.demoFacade.setDate(new Date());
+  }
+
+  public fetchApi() {
+    this.apiCallerService.callApi<DataModel[]>(this.simpleCallItem);
   }
 }
--- src/app/feature/demo/demo.module.ts
+++ src/app/feature/demo/demo.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatIconModule } from '@angular/material/icon';
+import { MatTableModule } from '@angular/material/table';
+import { ApiCallerModule } from '@deejayy/api-caller';
 import { DemoRoutingModule } from '@feature/demo/demo-routing.module';
 import { DemoFacade } from '@feature/demo/store/demo.facade';
 import { demoFeature } from '@feature/demo/store/demo.reducer';
@@ -12,11 +14,13 @@ import { DemoComponent } from './component/demo/demo.component';
 @NgModule({
   declarations: [DemoComponent],
   imports: [
+    ApiCallerModule,
     CommonModule,
     DemoRoutingModule,
     MatButtonModule,
     MatDividerModule,
     MatIconModule,
+    MatTableModule,
     StoreModule.forFeature(demoFeature),
   ],
   providers: [DemoFacade],
